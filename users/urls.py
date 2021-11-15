48b345f51b05ce7bbe3a7ed7b18c3e0d70ff252e
Dmitry Ratkevich <3bohu103@gmail.com>
2021-11-15 13:26:07 +0300

    created base django app with helps standard packages of auth users

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import dashboard
+
+
+urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('dashboard/', dashboard, name='dashboard'),
+]
